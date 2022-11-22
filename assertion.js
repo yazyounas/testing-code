3c62e9e27070aba94181c3581103c51a729d93cc
Yasir Younas <yazyounas@gmail.com>
2022-11-22 19:56:58 +0000

    npm testing

---
+++ assertion.js
@@ -0,0 +1,103 @@
+/*
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+		let expected = {a: 3, b: 4, result: 7};
+		let sum = {a: 3, b: 4};
+
+    // Exercise
+		sum.result = sum.a + sum.b;
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
+// Import assert here
+const assert = require('assert');
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.strictEqual(result, expected);
+  });
+});
+
+// Import assert here
+const assert = require('assert');
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.equal(result, expected);
+  });
+});
+// Import assert here
+const assert = require('assert');
+
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.ok(result === expected);
+  });
+});
+
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of its arguments', () => {
+
+    assert.ok(3 + 4 === 7);
+    
+  });
+});
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+		let expected = [3, 4, 7];
+		let sum = [3, 4];
+
+    // Exercise
+		sum.push(3 + 4);
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
+const assert = require('assert');
+
+describe('Numbers', () => {
+  it('1 does not equal 2', () => {
+    // Verify
+    assert.notStrictEqual(1, 2);
+  });
+});
+
+
+
+
+
+
+*/
--- mochatesting.js
+++ mochatesting.js
@@ -13,6 +13,7 @@ describe('Math', () => {
   });
   npm test
   */
+ /*
   const assert = require('assert');
 const fs = require('fs');
 let path, str;
@@ -64,3 +65,85 @@ describe('appendFileSync', () => {
   
  });
 });
+*/
+/*index.html
+Rooster = {};
+
+// Return a morning rooster call
+Rooster.announceDawn = () => {
+  return 'cock-a-doodle-doo!';
+}
+
+// Return hour as string
+// Throws Error if hour is not between 0 and 23 inclusive
+Rooster.timeAtDawn = (hour) => {
+  if (hour < 0 || hour > 23) {
+    throw new RangeError;
+  } else {
+    return hour.toString();
+  };
+}
+
+module.exports = Rooster;
+*/
+const assert = require('assert');
+const Rooster = require('../index');
+
+describe('Rooster', () => {
+  describe('.announceDawn', () => {
+    it('return a rooster call', () => {
+      //setup
+      const expected = 'cock-a-doodle-doo!';
+
+      //Excercise
+      const actual = Rooster.announceDawn();
+
+      //Verify
+      assert.equal(actual, expected);
+    });
+  });
+   describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+
+      const inputNumber = 12;
+      const expected = '12';
+
+      const actual = Rooster.timeAtDawn(inputNumber);
+      assert.equal(actual, expected);
+
+    });
+  });
+  describe('.timeAtDawn', () => {
+    it('throws an error if passed a number less than 0', () => {
+
+      const inputNumber = -1;
+      const expected = RangeError;
+
+      
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber)
+
+      }, expected);
+
+    });
+  });
+  describe('.timeAtDawn', () => {
+    it('throws an error if passed a number greater than 23', () => {
+
+      const inputNumber = 24;
+      const expected = RangeError;
+
+      
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber)
+
+      }, expected);
+
+    });
+  });
+  
+  
+  
+});
+
+ 
